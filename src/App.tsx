35071eb4e968be8d9fd1bc63fdbd97eeed478886
MartinXPN <mirakyanmartin@gmail.com>
2019-07-27 16:30:38 -0700

    made the grid responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {User} from "firebase";
 import firebase from 'firebase';
 import SortableComponent from "./annotation/List";
 // @ts-ignore
-import GridLayout from 'react-grid-layout';
+import { Responsive, WidthProvider} from 'react-grid-layout';
 
 interface Props {
 }
@@ -38,6 +38,8 @@ class App extends React.Component<Props, State> {
     }
 
     render(): React.ReactElement {
+        const ResponsiveGridLayout = WidthProvider(Responsive);
+
         return (
             <div>
                 <header className="App-header">
@@ -48,9 +50,14 @@ class App extends React.Component<Props, State> {
                         <div className="Sign-out-container"><SignOut/></div>}
                 </header>
 
-                <GridLayout className="Annotation-grid-container" cols={12} rowHeight={30} width={1200}>
-                    <SortableComponent key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}/>
-                </GridLayout>
+                <ResponsiveGridLayout
+                    className="Annotation-grid-container"
+                    breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
+                    cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
+
+                    <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 1, static: true}}><SortableComponent/></div>
+                    <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 1, static: true}}><SortableComponent/></div>
+                </ResponsiveGridLayout>
             </div>
         );
     }
