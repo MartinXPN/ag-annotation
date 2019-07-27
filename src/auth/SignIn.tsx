8992e5aaba6afa40679872df6ba1197fc69b06e6
MartinXPN <mirakyanmartin@gmail.com>
2019-07-27 15:36:52 -0700

    Added facebook auth

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -34,7 +34,7 @@ const uiConfig: firebaseui.auth.Config = {
     // We will display Google and Facebook as auth providers.
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     ]
 };
 
